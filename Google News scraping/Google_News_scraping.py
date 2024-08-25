5065edf5fc4b7e61bd8d99f81381b6d13a96ea18
johnson <youchengz921@gmail.com>
2024-08-25 12:51:39 +0800

    更新程式碼觀看內容更詳細

--- Google News scraping/Google_News_scraping.py
+++ Google News scraping/Google_News_scraping.py
@@ -1,5 +1,16 @@
 from scraparazzie import scraparazzie
 
-client = scraparazzie.NewsClient(language='chinese traditional', location='Taiwan', topic='Business', max_results=8)
+client = scraparazzie.NewsClient(language='chinese traditional', location='Taiwan', topic='Business', max_results=5)
 
-client.print_news()
+# client.print_news()
+
+items = client.export_news()
+print(len(items))
+
+for i, item in enumerate(items):
+    print("第" + str(i + 1) + "則新聞")
+    print("新聞標題: " + item['title'])
+    print("新聞機構: " + item['source'])
+    print("新聞連結: " + item['link'])
+    print("新聞時間: " + item['publish_date'])
+    print("==============================================================")
